a3c25c80143cfb65742f2694f570afb72b2a4521
tan666000 <nzdm54088@outlook.com>
2026-03-30 13:37:53 +0800

    Update sucompat.c

--- kernel/feature/sucompat.c
+++ kernel/feature/sucompat.c
@@ -11,7 +11,7 @@
 #include <linux/version.h>
 #include <linux/sched/task_stack.h>
 #include <linux/ptrace.h>
-#include <linux/susfs.h> // 新增：SUSFS 支持
+#include <linux/susfs.h>
 
 #include "arch.h"
 #include "policy/allowlist.h"
@@ -26,169 +26,173 @@
 #define SU_PATH "/system/bin/su"
 #define SH_PATH "/system/bin/sh"
 
+/* 确保声明 SUSFS 相关函数，防止 implicit declaration 报错 */
+#ifdef CONFIG_KSU_SUSFS
+extern bool susfs_is_current_is_sus_status(void);
+#else
+static inline bool susfs_is_current_is_sus_status(void) { return false; }
+#endif
+
 bool ksu_su_compat_enabled __read_mostly = true;
 
 static int su_compat_feature_get(u64 *value)
 {
-    *value = ksu_su_compat_enabled ? 1 : 0;
-    return 0;
+      *value = ksu_su_compat_enabled ? 1 : 0;
+      return 0;
 }
 
 static int su_compat_feature_set(u64 value)
 {
-    bool enable = value != 0;
-    ksu_su_compat_enabled = enable;
-    pr_info("su_compat: set to %d\n", enable);
-    return 0;
+      bool enable = value != 0;
+      ksu_su_compat_enabled = enable;
+      pr_info("su_compat: set to %d\n", enable);
+      return 0;
 }
 
 static const struct ksu_feature_handler su_compat_handler = {
-    .feature_id = KSU_FEATURE_SU_COMPAT,
-    .name = "su_compat",
-    .get_handler = su_compat_feature_get,
-    .set_handler = su_compat_feature_set,
+      .feature_id = KSU_FEATURE_SU_COMPAT,
+      .name = "su_compat",
+      .get_handler = su_compat_feature_get,
+      .set_handler = su_compat_feature_set,
 };
 
 static void __user *userspace_stack_buffer(const void *d, size_t len)
 {
-    char __user *p = (void __user *)current_user_stack_pointer() - len;
-    return copy_to_user(p, d, len) ? NULL : p;
+      char __user *p = (void __user *)current_user_stack_pointer() - len;
+      return copy_to_user(p, d, len) ? NULL : p;
 }
 
 static char __user *sh_user_path(void)
 {
-    static const char sh_path[] = "/system/bin/sh";
-    return userspace_stack_buffer(sh_path, sizeof(sh_path));
+      static const char sh_path[] = "/system/bin/sh";
+      return userspace_stack_buffer(sh_path, sizeof(sh_path));
 }
 
 static char __user *ksud_user_path(void)
 {
-    static const char ksud_path[] = KSUD_PATH;
-    return userspace_stack_buffer(ksud_path, sizeof(ksud_path));
+      static const char ksud_path[] = KSUD_PATH;
+      return userspace_stack_buffer(ksud_path, sizeof(ksud_path));
 }
 
 int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode, int *__unused_flags)
 {
-    const char su[] = SU_PATH;
+      const char su[] = SU_PATH;
 
-    // 新增：如果进程处于 SUSFS 状态，直接返回 ENOENT
-    if (susfs_is_current_is_sus_status()) {
-        return 0;
-    }
+      if (susfs_is_current_is_sus_status()) {
+            return 0;
+      }
 
-    if (!ksu_is_allow_uid_for_current(current_uid().val)) {
-        return 0;
-    }
+      if (!ksu_is_allow_uid_for_current(current_uid().val)) {
+            return 0;
+      }
 
-    char path[sizeof(su) + 1];
-    memset(path, 0, sizeof(path));
-    strncpy_from_user_nofault(path, *filename_user, sizeof(path));
+      char path[sizeof(su) + 1];
+      memset(path, 0, sizeof(path));
+      strncpy_from_user_nofault(path, *filename_user, sizeof(path));
 
-    if (unlikely(!memcmp(path, su, sizeof(su)))) {
-        pr_info("faccessat su->sh!\n");
-        *filename_user = sh_user_path();
-    }
+      if (unlikely(!memcmp(path, su, sizeof(su)))) {
+            pr_info("faccessat su->sh!\n");
+            *filename_user = sh_user_path();
+      }
 
-    return 0;
+      return 0;
 }
 
 int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags)
 {
-    const char su[] = SU_PATH;
+      const char su[] = SU_PATH;
 
-    // 新增：如果进程处于 SUSFS 状态，隐藏 su 路径
-    if (susfs_is_current_is_sus_status()) {
-        return 0;
-    }
+      if (susfs_is_current_is_sus_status()) {
+            return 0;
+      }
 
-    if (!ksu_is_allow_uid_for_current(current_uid().val)) {
-        return 0;
-    }
+      if (!ksu_is_allow_uid_for_current(current_uid().val)) {
+            return 0;
+      }
 
-    if (unlikely(!filename_user)) {
-        return 0;
-    }
+      if (unlikely(!filename_user)) {
+            return 0;
+      }
 
-    char path[sizeof(su) + 1];
-    memset(path, 0, sizeof(path));
-    strncpy_from_user_nofault(path, *filename_user, sizeof(path));
+      char path[sizeof(su) + 1];
+      memset(path, 0, sizeof(path));
+      strncpy_from_user_nofault(path, *filename_user, sizeof(path));
 
-    if (unlikely(!memcmp(path, su, sizeof(su)))) {
-        pr_info("newfstatat su->sh!\n");
-        *filename_user = sh_user_path();
-    }
+      if (unlikely(!memcmp(path, su, sizeof(su)))) {
+            pr_info("newfstatat su->sh!\n");
+            *filename_user = sh_user_path();
+      }
 
-    return 0;
+      return 0;
 }
 
 long ksu_handle_execve_sucompat(const char __user **filename_user, int orig_nr, const struct pt_regs *regs)
 {
-    const char su[] = SU_PATH;
-    const char __user *fn;
-    const char __user *const __user *argv_user = (const char __user *const __user *)PT_REGS_PARM2(regs);
-    struct ksu_sulog_pending_event *pending_sucompat = NULL;
-    char path[sizeof(su) + 1];
-    long ret;
-    unsigned long addr;
+      const char su[] = SU_PATH;
+      const char __user *fn;
+      const char __user *const __user *argv_user = (const char __user *const __user *)PT_REGS_PARM2(regs);
+      struct ksu_sulog_pending_event *pending_sucompat = NULL;
+      char path[sizeof(su) + 1];
+      long ret;
+      unsigned long addr;
 
-    // 新增：如果进程处于 SUSFS 状态，禁止通过 sucompat 执行
-    if (susfs_is_current_is_sus_status()) {
-        goto do_orig_execve;
-    }
+      if (susfs_is_current_is_sus_status()) {
+            goto do_orig_execve;
+      }
 
-    if (unlikely(!filename_user))
-        goto do_orig_execve;
+      if (unlikely(!filename_user))
+            goto do_orig_execve;
 
-    if (!ksu_is_allow_uid_for_current(current_uid().val))
-        goto do_orig_execve;
+      if (!ksu_is_allow_uid_for_current(current_uid().val))
+            goto do_orig_execve;
 
-    addr = untagged_addr((unsigned long)*filename_user);
-    fn = (const char __user *)addr;
-    memset(path, 0, sizeof(path));
+      addr = untagged_addr((unsigned long)*filename_user);
+      fn = (const char __user *)addr;
+      memset(path, 0, sizeof(path));
 
-    ret = strncpy_from_user(path, fn, sizeof(path));
+      ret = strncpy_from_user(path, fn, sizeof(path));
 
-    if (ret < 0) {
-        pr_warn("Access filename when execve failed: %ld", ret);
-        goto do_orig_execve;
-    }
+      if (ret < 0) {
+            pr_warn("Access filename when execve failed: %ld", ret);
+            goto do_orig_execve;
+      }
 
-    if (likely(memcmp(path, su, sizeof(su))))
-        goto do_orig_execve;
+      if (likely(memcmp(path, su, sizeof(su))))
+            goto do_orig_execve;
 
-    pr_info("sys_execve su found\n");
-    pending_sucompat = ksu_sulog_capture_sucompat(*filename_user, argv_user, GFP_KERNEL);
-    *filename_user = ksud_user_path();
+      pr_info("sys_execve su found\n");
+      pending_sucompat = ksu_sulog_capture_sucompat(*filename_user, argv_user, GFP_KERNEL);
+      *filename_user = ksud_user_path();
 
-    ret = escape_with_root_profile();
-    if (ret) {
-        pr_err("escape_with_root_profile failed: %ld\n", ret);
-        ksu_sulog_emit_pending(pending_sucompat, ret, GFP_KERNEL);
-        goto do_orig_execve;
-    }
+      ret = escape_with_root_profile();
+      if (ret) {
+            pr_err("escape_with_root_profile failed: %ld\n", ret);
+            ksu_sulog_emit_pending(pending_sucompat, ret, GFP_KERNEL);
+            goto do_orig_execve;
+      }
 
-    ret = ksu_syscall_table[orig_nr](regs);
-    if (ret < 0) {
-        pr_err("failed to execve ksud as su: %ld, fallback to sh\n", ret);
-        ksu_sulog_emit_pending(pending_sucompat, ret, GFP_KERNEL);
-        *filename_user = sh_user_path();
-    } else {
-        ksu_sulog_emit_pending(pending_sucompat, ret, GFP_KERNEL);
-        return ret;
-    }
+      ret = ksu_syscall_table[orig_nr](regs);
+      if (ret < 0) {
+            pr_err("failed to execve ksud as su: %ld, fallback to sh\n", ret);
+            ksu_sulog_emit_pending(pending_sucompat, ret, GFP_KERNEL);
+            *filename_user = sh_user_path();
+      } else {
+            ksu_sulog_emit_pending(pending_sucompat, ret, GFP_KERNEL);
+            return ret;
+      }
 
 do_orig_execve:
-    return ksu_syscall_table[orig_nr](regs);
+      return ksu_syscall_table[orig_nr](regs);
 }
 
-void ksu_sucompat_init()
+void ksu_sucompat_init(void)
 {
-    if (ksu_register_feature_handler(&su_compat_handler)) {
-        pr_err("Failed to register su_compat feature handler\n");
-    }
+      if (ksu_register_feature_handler(&su_compat_handler)) {
+            pr_err("Failed to register su_compat feature handler\n");
+      }
 }
 
-void ksu_sucompat_exit()
+void ksu_sucompat_exit(void)
 {
-    ksu_unregister_feature_handler(KSU_FEATURE_SU_COMPAT);
+      ksu_unregister_feature_handler(KSU_FEATURE_SU_COMPAT);
 }
